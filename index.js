a4684eca04543b1eb7edf4c114eec1634a643e2e
James Arn <132308884+arnsocks@users.noreply.github.com>
2025-10-03 11:24:24 -0400

    Implement testing for string reversal and pass

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ export function capitalize(x) {
     return i + x.slice(1)
 }
 
+export function reverseString(input) {
+  let output = '';
+  for (let i = input.length-1; i >= 0; i--) {
+    output += input[i];
+    // console.log(input[i])
+  }
+  return output;
+}
+
+console.log(reverseString('PeAnuTButTeR'));
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import {capitalize} from './index.js'
+import {capitalize, reverseString} from './index.js'
 
 describe('Capitalization', () => {
   test("dog becomes Dog",() => {
@@ -8,4 +8,18 @@ describe('Capitalization', () => {
   test("a becomes A", () => {
     expect(capitalize('a')).toBe('A');
   }) 
+});
+
+describe('Reverse string', () => {
+  test('james becomes semaj', () => {
+    expect(reverseString('james')).toBe('semaj');
+  });
+
+  test('a becomes a', () => {
+    expect(reverseString('a')).toBe('a');
+  });
+
+  test('PeAnuT ButTeR becomes ReTtuB TunAeP', () => {
+    expect(reverseString("PeAnuT ButTeR")).toBe("ReTtuB TunAeP");
+  });
 });
